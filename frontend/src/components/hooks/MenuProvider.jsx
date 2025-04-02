50d0cdcde134d09677a0bd6a17fdbf722419769e
Teddy <teddytilahun02@gmail.com>
2025-04-02 22:30:06 +0300

    delete unused files

--- frontend/src/components/hooks/MenuProvider.jsx
+++ frontend/src/components/hooks/MenuProvider.jsx
@@ -2,7 +2,6 @@ import { createContext, useState } from "react";
 
 export const MenuContext = createContext();
 
-// eslint-disable-next-line react/prop-types
 export const MenuProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
