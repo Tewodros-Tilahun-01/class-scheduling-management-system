52f44b8d421e194c4699d651ed02884ba92065e6
Teddy <teddytilahun02@gmail.com>
2025-04-25 21:26:52 +0300

    making the schedule frontend

--- frontend/src/pages/Activity.jsx
+++ frontend/src/pages/Activity.jsx
@@ -1,7 +1,13 @@
+import ScheduleForm from "@/components/custom/ScheduleForm";
 import React from "react";
 
 function Activity() {
-  return <div>Activity</div>;
+  return (
+    <div>
+      Activity
+      <ScheduleForm />
+    </div>
+  );
 }
 
 export default Activity;
