a0595a944142d09e35785a04dd694961acf9f5c6
Teddy <teddytilahun02@gmail.com>
2025-05-11 13:57:35 +0300

    remove expectedEnrollment

--- backend/models/StudentGroup.js
+++ backend/models/StudentGroup.js
@@ -4,7 +4,6 @@ const studentGroupSchema = new mongoose.Schema({
   department: { type: String, required: true },
   year: { type: Number, required: true },
   section: { type: String, required: true },
-  expectedEnrollment: { type: Number, required: true, min: 1 },
   isDeleted: { type: Boolean, default: false, index: true },
 });
 
